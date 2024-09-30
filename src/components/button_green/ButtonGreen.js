296ed16ec0348c2462fa9662f00784391a2859d0
grachev <grachev613@gmail.com>
2024-09-30 16:07:15 +0400

    green button

--- src/components/button_green/ButtonGreen.js
+++ src/components/button_green/ButtonGreen.js
@@ -1,13 +1,39 @@
 import "./styles.css";
 import back from "../../media/icons/back_3710800.png";
+import { useState, useEffect } from "react";
 
 const ButtonGreen = () => {
+
+  //В первой переменной стартовое состояние
+  // Во второй переменной функция которая меняет состояние
+  const [green, setGreen] = useState('off')
+
+  useEffect(() => {
+    if (green === 'on') {
+      document.querySelector('.ButtonGreen> img').classList.add('shift')
+    }
+
+    if (green === 'off') {
+      document.querySelector('.ButtonGreen> img').classList.remove('shift')
+    }
+
+  }, [green])
+
+  const onMous = () => {
+    setGreen('on')
+  }
+
+  const offMous = () => {
+    setGreen('off')
+
+  }
+
   return (
-    <div className="ButtonGreen" onMouseEnter={} onMouseLeave={}>
-      <div className="button">
-        <img src={back} img />
+    <div className="ButtonGreen" onMouseEnter={onMous} onMouseLeave={offMous}>
+      <img src={back} />
+      <p>
         Создать заказ
-      </div>
+      </p>
     </div>
   );
 }
